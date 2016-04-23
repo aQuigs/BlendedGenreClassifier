1222d8059cf36f6e804cf3fb3c985cd1d76c56cc
aQuigs <aQuigs@users.noreply.github.com>
2016-04-22 20:21:32 -0400

    Implement 10-fold cross validation

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -37,7 +37,7 @@ GENRE_DICT = {
 NUMBER_OF_GENRES = len(GENRE_DICT)
 INPUT_DIMS = (26)*4
 
-def classifySegments():
+def classifySegments(trainingCSVs, testingCSVs):
     global NUMBER_OF_GENRES
     global INPUT_DIMS
     global GENRE_DICT
@@ -46,14 +46,13 @@ def classifySegments():
     SEGMENT_LENGTH = 1000
     PROCESSING_FILENAME = DIR + '/processing.wav'
     # AMP_PROCESSING_FILENAME = DIR + '/processing.amp'
-    TRAINING_DATA_PROPORTION = 0.8
+    # TRAINING_DATA_PROPORTION = 0.8
     TRAINING_EPOCHS = 80
 
     print('Reading training data...')
     trndata_temp = ClassificationDataSet(INPUT_DIMS, 1, nb_classes=NUMBER_OF_GENRES)
-    g = glob.glob(DIR + '/*.csv')
-    shuffle(g)
-    for filename in g[:int(TRAINING_DATA_PROPORTION*len(g))]:
+
+    for filename in trainingCSVs:
         basename = os.path.splitext(filename)[0]
         data = None
         genre = None
@@ -111,7 +110,7 @@ def classifySegments():
     genreSongCount = [0] * NUMBER_OF_GENRES
     correctlyClassifiedSongCount = [0] * NUMBER_OF_GENRES
     averageSegmentAccuracies=[0] * NUMBER_OF_GENRES
-    for filename in g[int(TRAINING_DATA_PROPORTION*len(g)):]:
+    for filename in testingCSVs:
         basename = os.path.splitext(os.path.basename(filename))[0]
         print('Processing ' + basename + '...')
         song = AudioSegment.from_wav(SONG_FILE_DIR + '/' + basename + '.wav')
@@ -179,6 +178,9 @@ def classifySegments():
     print('Total songs tested: %d' % totalSongCount)
     print('Average segment classification accuracy per song: %5.2f%%' % (100.0*totalAccuracy/totalSongCount))
     print('Total accuracy for properly identified songs: %5.2f%%' % (100.0*correctlyClassifiedSongs/totalSongCount))
+    print('='*75)
+    genreSongCount = [1 if i == 0 else i for i in genreSongCount]
+    return [correctlyClassifiedSongCount[i]/float(genreSongCount[i]) for i in xrange(NUMBER_OF_GENRES)], [averageSegmentAccuracies[i]/genreSongCount[i] for i in xrange(NUMBER_OF_GENRES)]
 
 ## Deprecated
 # def classifyWhole():
@@ -226,5 +228,36 @@ def classifySegments():
 #               "  train error: %5.2f%%" % trnresult, \
 #               "  test error: %5.2f%%" % tstresult
 
+# Uses 10-fold cross validation
+def generateAccuracies():
+    global DIR
+    global NUMBER_OF_GENRES
+
+    N = 10
+
+    g = glob.glob(DIR + '/*.csv')
+    # g.sort()
+    shuffle(g)
+
+    DATA_LENGTH = len(g)/N
+    songAccuracies = [0]*NUMBER_OF_GENRES
+    segmentAccuracies = [0]*NUMBER_OF_GENRES
+    for i in xrange(N):
+        startIndex = i*DATA_LENGTH
+        endIndex = len(g) if i == N else (i+1)*DATA_LENGTH
+        (songAccuracyIncr, segmentAccuracyIncr) = classifySegments(g[:startIndex] + g[endIndex:], g[startIndex:endIndex])
+        songAccuracies = [songAccuracies[i] + songAccuracyIncr[i] for i in xrange(NUMBER_OF_GENRES)]
+        segmentAccuracies = [segmentAccuracies[i] + segmentAccuracyIncr[i] for i in xrange(NUMBER_OF_GENRES)]
+
+    songAccuracies = [x/N for x in songAccuracies]
+    segmentAccuracies = [x/N for x in segmentAccuracies]
+
+    print('|'*100)
+    for k in GENRE_DICT:
+        i = GENRE_DICT[k]
+        print('Cross-Validated song classification accuracy for %s: %5.2f%%' % (k, 100.0*songAccuracies[i]))
+        print('Cross-Validated segment classification accuracy for %s: %5.2f%%' % (k, 100.0*segmentAccuracies[i]))
+    print('|'*100)
+
 if __name__ == '__main__':
-    classifySegments()
+    generateAccuracies()
